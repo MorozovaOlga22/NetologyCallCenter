3fd49d4d709d5b408fec875e11e83a32cd8619fc
Olga Morozova <Morozova_Olga_22@mail.ru>
2021-10-28 03:43:40 +0300

    Init commit

---
+++ src/main/java/ru/netology/Call.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Call {
+    private final int callNumber;
+
+    public Call(int callNumber) {
+        this.callNumber = callNumber;
+    }
+
+    public int getCallNumber() {
+        return callNumber;
+    }
+}
---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,86 @@
+package ru.netology;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Main {
+    private static final int TIMES_TO_CALL = 3;
+    private static final int CALLS_COUNT = 60;
+    private static final int TIME_BETWEEN_CALLS = 1_000;
+
+    private static final int TIME_BETWEEN_CHECK_CALL = 100;
+    private static final int TIME_TO_HANDLE_CALL = 3_000;
+    private static final int OPERATORS_COUNT = 5;
+
+    //Рассчитываем, что после этого количества уже нет смысла ставить звонки в очередь (пусть лучше перезвонят)
+    private static final int MAX_CALLS_COUNT = 1_000;
+
+    private static volatile boolean noMoreCalls;
+
+    public static void main(String[] args) throws InterruptedException {
+        final Queue<Call> callQueue = new LinkedBlockingQueue<>(MAX_CALLS_COUNT);
+
+        final List<Thread> operatorsList = new ArrayList<>();
+        for (int i = 0; i < OPERATORS_COUNT; i++) {
+            final Thread operatorThread = new Thread(getOperatorAction(callQueue), "Оператор" + (i + 1));
+            operatorsList.add(operatorThread);
+            operatorThread.start();
+        }
+
+        final Thread atcThread = new Thread(getAtcAction(callQueue));
+        atcThread.start();
+
+        atcThread.join();
+        operatorsList.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println("Колл-центр закрывается");
+    }
+
+    @SuppressWarnings("BusyWait")
+    private static Runnable getOperatorAction(Queue<Call> callQueue) {
+        return () -> {
+            while (true) {
+                final Call call = callQueue.poll();
+                try {
+                    if (call == null) {
+                        if (noMoreCalls) {
+                            System.out.println(Thread.currentThread().getName() + " идет домой");
+                            return;
+                        }
+                        Thread.sleep(TIME_BETWEEN_CHECK_CALL);
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " обрабатывает звонок " + call.getCallNumber());
+                        Thread.sleep(TIME_TO_HANDLE_CALL);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    private static Runnable getAtcAction(Queue<Call> callQueue) {
+        return () -> {
+            for (int i = 0; i < TIMES_TO_CALL; i++) {
+                for (int j = 0; j < CALLS_COUNT; j++) {
+                    final int callNumber = i * CALLS_COUNT + j;
+                    System.out.println("Entered call " + callNumber);
+                    callQueue.add(new Call(callNumber));
+                }
+                try {
+                    Thread.sleep(TIME_BETWEEN_CALLS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            noMoreCalls = true;
+        };
+    }
+}
